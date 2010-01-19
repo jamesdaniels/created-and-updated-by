8cf153d5d2889eb269f70ca24bd85a4994d28d1d
James Daniels <james@jamesdaniels.net>
2010-01-18 21:47:07 -0500

    Cleaning things up, created_by and updated_by are no longer associations

--- lib/created-and-updated-by.rb
+++ lib/created-and-updated-by.rb
@@ -1,34 +1,20 @@
-module CreatedAndUpdatedBy
-	
+module CreatedAndUpdatedBy	
 	class Stamper
 		cattr_accessor :stampable, :attribute
 		def self.attach(stamp_model = User, stamp_attribute = :current)
 			raise ArgumentError, "#{stamp_model.name} does not respond to #{stamp_attribute}" unless stamp_model.respond_to?(stamp_attribute)
 			self.stampable, self.attribute = stamp_model, stamp_attribute
-			ActiveRecord::Base.extend CreatedAndUpdatedBy  
-			ActiveRecord::Base.send(:acts_as_stampable)
+			ActiveRecord::Base.send(:include, CreatedAndUpdatedBy)
 		end
 	end
-	
-	def acts_as_stampable
-		class_eval do
-			send :include, InstanceMethods
-			before_validation :set_stamps
-			belongs_to :updated_by, :class_name => CreatedAndUpdatedBy::Stamper.stampable.name
-			belongs_to :created_by, :class_name => CreatedAndUpdatedBy::Stamper.stampable.name
-		end
-	end
-	
-	module InstanceMethods
+	class_eval do
 		def stamper
 			CreatedAndUpdatedBy::Stamper.stampable.send(CreatedAndUpdatedBy::Stamper.attribute)
 		end
+		def updated_by; CreatedAndUpdatedBy::Stamper.stampable.find(updated_by_id); end
+		def created_by; CreatedAndUpdatedBy::Stamper.stampable.find(updated_by_id); end
 		def set_stamps
-			if stamper && respond_to?(:created_by_id) && respond_to?(:updated_by_id)
-				self.created_by ||= stamper
-				self.updated_by = stamper
-			end
+			self.created_by_id ||= self.updated_by_id = stamper.id if stamper && respond_to?(:created_by_id) && respond_to?(:updated_by_id)
 		end
-	end   
-	
+	end	
 end
